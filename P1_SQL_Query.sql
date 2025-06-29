9bdfd5b56c2cd3061422aefa8e553110e38d8c23
ujjwalbabu0 <91885668+ujjwalbabu0@users.noreply.github.com>
2025-06-29 19:20:32 +0530

    Update P1_SQL_Query.sql

--- P1_SQL_Query.sql
+++ P1_SQL_Query.sql
@@ -53,6 +53,19 @@ WHERE
 
 -- 3. Data Analysis & Findings
 
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of November 2022
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+-- Q.5 Write a SQL query to find all transactions where the total sale is greater than 1000.
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out the best-selling month in each year
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+-- Q.10 Write a SQL query to create each shift and number of orders (Example: Morning <=12, Afternoon Between 12 & 17, Evening >17)
+
+
+
 -- (1)Write a SQL query to retrieve all columns for sales made on '2022-11-05:
 select * from retail_sales
 where sale_Date = '2022-11-05';
